a8c41c1a36e24a60a233b7a3bd9034e32215cbba
Андрей Скоробогатов <ufo1og@yandex.ru>
2022-03-01 17:57:24 +0500

    добавил различные методы сортировки

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,56 @@
-import java.util.Arrays;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Random;
 
 public class Main {
-    public static final int[] arrayToSort = {18, 6, 3, 13, 99, 123, 12, 95, 64, 42, 55, 17, 36};
+    public static final int arraySize = 100000;
+    public static final int arrayMaxValue = 10;
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(arrayToSort));
-        System.out.println(Arrays.toString(Sorter.bubbleSort(arrayToSort)));
-        System.out.println(Arrays.toString(arrayToSort));
-        System.out.println(Arrays.toString(Sorter.selectionSort(arrayToSort)));
+        int[] arrayToSort = generateArray(arraySize);
+        Instant start;
+        Instant end;
+        int[] array = Sorter.createCopyOfArray(arrayToSort);
+        start = Instant.now();
+        Sorter.quickSort(array, 0, array.length - 1);
+        end = Instant.now();
+        System.out.println("Быстрая сортировка: " + Duration.between(start, end).toMillis() + " мс");
+
+        array = Sorter.createCopyOfArray(arrayToSort);
+        start = Instant.now();
+        Sorter.bubbleSort(array);
+        end = Instant.now();
+        System.out.println("Сортировка пузырьком: " + Duration.between(start, end).toMillis() + " мс");
+
+        array = Sorter.createCopyOfArray(arrayToSort);
+        start = Instant.now();
+        Sorter.selectionSort(array);
+        end = Instant.now();
+        System.out.println("Сортировка вставкой: " + Duration.between(start, end).toMillis() + " мс");
+
+        array = Sorter.createCopyOfArray(arrayToSort);
+        start = Instant.now();
+        Sorter.doubleInsertionSort(array);
+        end = Instant.now();
+        System.out.println("Сортировка двойной вставкой: " + Duration.between(start, end).toMillis() + " мс");
+
+        array = Sorter.createCopyOfArray(arrayToSort);
+        start = Instant.now();
+        Sorter.countSort(array, arrayMaxValue);
+        end = Instant.now();
+        System.out.println("Сортировка подсчетом: " + Duration.between(start, end).toMillis() + " мс");
 
     }
+
+    public static int[] generateArray (int length) {
+        int[] result = new int[length];
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++) {
+            result[i] = random.nextInt(arrayMaxValue);
+        }
+        return result;
+    }
+
 
 }
--- src/Sorter.java
+++ src/Sorter.java
@@ -1,5 +1,5 @@
 public class Sorter {
-    private static int[] createCopyOfArray (int[] array) {
+    public static int[] createCopyOfArray (int[] array) {
         int[] copyArray = new int[array.length];
         System.arraycopy(array, 0, copyArray, 0, copyArray.length);
         return copyArray;
@@ -11,40 +11,110 @@ public class Sorter {
         array[firstIndex] = array[secondIndex];
         array[secondIndex] = temp;
     }
-
-    public static int[] bubbleSort (int[] array) {
-        int[] result = createCopyOfArray(array);
-        int iterations = 0;
-
-        boolean needIteraion = true;
-        while (needIteraion) {
-            needIteraion = false;
-            for (int i = 1; i < result.length; i++) {
-                if (result[i] < result[i - 1]) {
-                    needIteraion = true;
-                    swapElements(result, i, i - 1);
-                    iterations++;
+    // Пузырьковая сортировка
+    public static void bubbleSort (int[] array) {
+        boolean needIteraction = true;
+        while (needIteraction) {
+            needIteraction = false;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    needIteraction = true;
+                    swapElements(array, i, i - 1);
                 }
             }
         }
-        System.out.println(iterations + " iterations");
-        return result;
     }
-
-    public static int[] selectionSort (int[] array) {
-        int[] result = createCopyOfArray(array);
-        int iteractions = 0;
-        for (int left = 0; left < result.length; left++) {
+    // Сортировка выбором
+    public static void selectionSort (int[] array) {
+        for (int left = 0; left < array.length; left++) {
             int minInd = left;
-            for (int i = minInd; i < result.length; i++) {
-                if (result[i] < result[minInd]) {
+            for (int i = minInd; i < array.length; i++) {
+                if (array[i] < array[minInd]) {
                     minInd = i;
                 }
-                iteractions++;
             }
-            swapElements(result, left, minInd);
+            swapElements(array, left, minInd);
         }
-        System.out.println(iteractions + " iterations");
-        return result;
     }
+    // Сортировка вставкой
+    public static void insertionSort (int[] array) {
+        for (int left = 1; left < array.length; left++) {
+            int current = array[left];
+            int j = left;
+            while (j > 0 && array[j - 1] > current) {
+                array[j] = array[j - 1];
+                j--;
+            }
+            array[j] = current;
+        }
+    }
+    // Сортировка двойной вставкой
+    public static void doubleInsertionSort (int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int left = 0;
+            int right = i - 1;
+
+            while (left <= right) {
+                int mid = left + ((right - left) >> 1);
+                if (key < array[mid]) {
+                    right = mid - 1;
+                } else
+                {
+                    left = mid + 1;
+                }
+            }
+            int end = i - 1;
+            while (end >= left) {
+                array[end + 1] = array[end];
+                end--;
+            }
+            array[left] = key;
+        }
+    }
+    // Быстрая сортировка
+    public static void quickSort (int[] array, int leftBorder, int rightBorder) {
+        int leftMarker = leftBorder;
+        int rightMarker = rightBorder;
+        int pivot = array[(leftMarker + rightMarker) / 2];
+
+        do {
+            while (array[leftMarker] < pivot) {
+                leftMarker++;
+            }
+            while (array[rightMarker] > pivot) {
+                rightMarker--;
+            }
+            if (leftMarker <= rightMarker) {
+                if (leftMarker < rightMarker) {
+                    swapElements(array, leftMarker, rightMarker);
+                }
+                leftMarker++;
+                rightMarker--;
+            }
+        } while (leftMarker <= rightMarker);
+
+        if (leftMarker < rightBorder) {
+            quickSort(array, leftMarker, rightBorder);
+        }
+        if (leftBorder < rightMarker) {
+            quickSort(array, leftBorder, rightMarker);
+        }
+    }
+    // Сортировка подсчетом
+    public static void countSort (int[] array, int maxValue) {
+        int[] count = new int[maxValue + 1];
+        for (int i = 0; i < array.length; i++) {
+            count[array[i]]++;
+        }
+        int index = 0;
+        for (int i = 0; i < count.length; i++) {
+            for (int j = 0; j < count[i]; j++) {
+                array[index] = i;
+                index++;
+            }
+        }
+    }
+
+
 }
